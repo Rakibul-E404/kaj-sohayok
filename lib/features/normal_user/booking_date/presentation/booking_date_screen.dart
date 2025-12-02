91ba05ec71006fa4f31f65796be77f9d55ac5a37
ImtiazChowdhury <work.imtiazchowdhury@gmail.com>
2025-12-02 12:55:01 +0600

    Screen : Booking Date, Phase : Api Integration, Status : On-Going

--- lib/features/normal_user/booking_date/presentation/booking_date_screen.dart
+++ lib/features/normal_user/booking_date/presentation/booking_date_screen.dart
@@ -1,30 +1,32 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:kaz_bd/features/normal_user/booking_date/presentation/widgets/build_proceed_button.dart';
 import 'package:kaz_bd/features/normal_user/booking_date/presentation/widgets/calender_container_widget.dart';
 import 'package:kaz_bd/features/normal_user/booking_date/presentation/widgets/time_picker_widget.dart';
 import 'package:kaz_bd/helpers/ui_helpers.dart';
 
 import '../../../../constants/text_font_style.dart';
 import '../../../../controllers/calender_controller.dart';
+import '../../../../custom_widgets/custom_elevated_button.dart';
+import '../../../../gen/colors.gen.dart';
+import '../../../../routes/routes.dart';
 
 class BookingDateScreen extends StatelessWidget {
   const BookingDateScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final arguments = Get.arguments as Map<String, dynamic>?;
+    final providerId = arguments?['providerId'] ?? '';
+
     ///------------------------------------------- Initialize the controller
     final CalendarController controller = Get.find<CalendarController>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            // todo: back function
-          },
-          icon: Icon(Icons.arrow_back),
-        ),
+        automaticallyImplyLeading: true,
         title: Text(
           "Booking Date",
           style: TextFontStyle.headline18w700c000000StyleSatoshi,
@@ -42,7 +44,16 @@ class BookingDateScreen extends StatelessWidget {
               TimePickerWidget(),
               Spacer(),
 
-              BuildProceedButton(),
+              CustomElevatedButton(
+                onTap: () {
+                  Get.toNamed(Routes.searchLocationScreen);
+                  log("Proceed button pressed");
+                },
+                buttonTitle: "Proceed",
+                textStyle: TextFontStyle.headline16w700cFFFFFFStyleSatoshi,
+                buttonColor: AppColors.c778beb,
+                borderRadius: 12.r,
+              ),
             ],
           ),
         ),
