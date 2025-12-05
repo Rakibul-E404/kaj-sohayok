5e7e6862712c291ca170f89680a638fdd6a03ce9
Rakibul-E404 <smrat246@gmail.com>
2025-12-05 11:08:23 +0600

    workign with the card data passing

--- lib/features/service_provider/svp_work_completed/presentation/svp_work_completed_screen.dart
+++ lib/features/service_provider/svp_work_completed/presentation/svp_work_completed_screen.dart
@@ -1,3 +1,4 @@
+/**
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -48,6 +49,141 @@ class SvpWorkCompletedScreen extends StatelessWidget {
               );
             },
           ),
+        ),
+      ),
+    );
+  }
+}
+*/
+
+
+
+
+
+
+///
+///
+///
+/// todo:: fetching form the api
+///
+///
+///
+
+
+
+// lib/.../svp_work_completed_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../../../constants/text_font_style.dart';
+import '../../../../gen/colors.gen.dart';
+import '../../../../helpers/ui_helpers.dart';
+import '../controller/svp_work_completed_screen_controller.dart';
+
+class SvpWorkCompletedScreen extends StatelessWidget {
+  const SvpWorkCompletedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(SvpWorkCompletedScreenController());
+
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text(
+          "Work Completed",
+          style: TextFontStyle.headline18w700c000000StyleSatoshi,
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.scaffoldBackgroundColor,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Obx(() => _buildContent(controller)),
+      ),
+    );
+  }
+
+  Widget _buildContent(SvpWorkCompletedScreenController controller) {
+    if (controller.isLoading.value) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(color: AppColors.c000e08),
+            UIHelper.verticalSpace(16.h),
+            Text(
+              'Loading completed work...',
+              style: TextFontStyle.headline10w400c6c606cStyleSatoshi,
+            ),
+          ],
+        ),
+      );
+    }
+
+    if (controller.hasError.value) {
+      return Center(
+        child: Padding(
+          padding: EdgeInsets.all(20.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.error_outline, size: 50.h, color: Colors.red),
+              UIHelper.verticalSpace(16.h),
+              Text(
+                controller.errorMessage.value,
+                style: TextFontStyle.headline10w400c6c606cStyleSatoshi.copyWith(color: Colors.red),
+                textAlign: TextAlign.center,
+              ),
+              UIHelper.verticalSpace(16.h),
+              ElevatedButton(
+                onPressed: () => controller.fetchCompletedBookings(),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.c000e08,
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text('Retry'),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    if (controller.completedBookings.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.check_circle, size: 60.h, color: Colors.grey),
+            UIHelper.verticalSpace(16.h),
+            Text(
+              'No completed work yet',
+              style: TextFontStyle.headline10w500c000000StyleSatoshi.copyWith(color: Colors.grey),
+            ),
+            UIHelper.verticalSpace(8.h),
+            Text(
+              'Finished jobs will appear here',
+              style: TextFontStyle.headline10w400c6c606cStyleSatoshi.copyWith(color: Colors.grey),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return RefreshIndicator(
+      onRefresh: () => controller.fetchCompletedBookings(),
+      color: AppColors.c000e08,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: UIHelper.kDefaulutPadding()),
+        child: ListView.separated(
+          itemCount: controller.completedBookings.length,
+          separatorBuilder: (context, index) => UIHelper.verticalSpace(24.h),
+          itemBuilder: (context, index) {
+            return controller.buildCompletedBookingWidget(index);
+          },
         ),
       ),
     );
