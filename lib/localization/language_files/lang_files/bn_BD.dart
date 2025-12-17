3754c2d83a64889d9a1bad98ab9df5bd26b030d9
ImtiazChowdhury <work.imtiazchowdhury@gmail.com>
2025-12-17 11:09:51 +0600

    Screen : Sign In, Section: Localization Added, Status : Done

--- lib/localization/language_files/lang_files/bn_BD.dart
+++ lib/localization/language_files/lang_files/bn_BD.dart
@@ -1,12 +1,29 @@
 Map<String, String> banglaLanguage = {
+  ///----------> On-Boarding Screen
   'your_home_best_friend': 'তোমার বাড়ির সবচেয়ে ভালো বন্ধু',
   'get_your_home_services_done':
       'আপনার বাড়ির সমস্ত পরিষেবা এক জায়গায় পান: এসি মেরামত, প্লাম্বিং, পরিষ্কার, বৈদ্যুতিক এবং রঙ - মাত্র এক ট্যাপ দূরে।',
   'get_started': 'শুরু করুন',
+
+  ///----------> Choose Role Screen
   'skip': 'এড়িয়ে যান',
   'choose_role': 'নীচে আপনার ভূমিকা চয়ন করুন',
   'select_an_option':
       'আপনার যাত্রা শুরু করার জন্য অনুগ্রহ করে একটি বিকল্প নির্বাচন করুন।',
   'join_as_user': 'একজন ব্যবহারকারী হিসেবে যোগ দিন',
   'join_as_provider': 'পরিষেবা প্রদানকারী হিসেবে যোগদান করুন',
+
+  ///----------> Sign In Screen
+  'sign_in_to_account': 'আপনার অ্যাকাউন্ট সাইন ইন করুন',
+  'welcome_back': 'আবার স্বাগতম! আপনার বিবরণ লিখুন।',
+  'your_email': 'আপনার ইমেইল',
+  'enter_your_email': 'আপনার ইমেইল লিখুন',
+  'please_enter_your_email': 'আপনার ইমেল লিখুন দয়া করে',
+  'password': 'পাসওয়ার্ড',
+  'enter_password': 'পাসওয়ার্ড লিখুন',
+  'please_enter_password': 'আপনার পাসওয়ার্ড লিখুন',
+  'forgot_password': 'পাসওয়ার্ড ভুলে গেছেন?',
+  'sign_in': 'সাইন ইন করুন',
+  'don\'t_have_account': 'অ্যাকাউন্ট নেই?',
+  'sign_up': 'সাইন আপ করুন',
 };
